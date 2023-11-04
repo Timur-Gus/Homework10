9c9f0dc23ba33f57e90da1f6798d1933debe0070
Timur-Gus <timurka24478@gmail.ru>
2023-11-04 15:34:27 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     static void task1(){
         String firstName = "Ivan";
@@ -8,5 +9,9 @@ public class Main {
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("Ф. И. О. сотрудника — " + fullName);
+    }
+    static void task2(){
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " +fullName.toUpperCase());
     }
 }
